4b505d1d540273a7c5fbd8ba079b55531e28cc42
ROBERT ANGEL TORRES <rtorres61595@gmail.com>
2020-06-08 17:16:59 -0400

    started the logic

---
+++ db/store.js
@@ -0,0 +1,48 @@
+const db = require("./db.json")
+const fs = require("fs")
+const uuidv1 = require("uuid/v1")
+const util = require("util")
+
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store{
+    read(){
+        return readFileAsync("db/db.json", "utf8")
+    }
+    write(note){
+        return writeFileAsync("db/db.json", JSON.stringify(note))
+    }
+    getNote(){
+        return this.read().then((notes) => {
+            let parsedNotes = [];
+            try {
+                parsedNotes.concat(JSON.parse(notes));
+                console.log(parsedNotes)
+            } catch (err) {
+                parsedNotes = [];
+            }   
+            return parsedNotes;
+
+
+        })
+     }
+     addNote(note){
+        const { title, text } = note;
+        if(!title || !text){
+            throw new Error("can not be blank")
+        }
+        const newnote = { title, text, id: uuidv1() }
+        return this.getNote()
+        .then((notes) => [...notes, newnote])
+        .then((updatedNotes) => this.write(updatedNotes))
+        .then(() => newnote)
+    }
+    removeNote(id){
+        return this.getNote()
+        .then((notes)=> notes.filter((note) => note.id !== id))
+        .then((filteredNotes) => this.write(filteredNotes))
+    }
+}
+module.exports  = new Store()
---
+++ routes/apiRoutes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const store = require("../db/store")
+
+//router.get returns all notes
+
+//routher.post addNote(req.body)
+
+//routher.delete (:id) = req.params.id
+
+//don't forget to export
